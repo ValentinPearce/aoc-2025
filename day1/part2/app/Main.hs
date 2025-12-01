e04e84c8f7eb2c0129bd5b7b5b7ef51d8a06cf87
Valentin Pearce <valentin.pearce@forgetfulmuffin.fr>
2025-12-02 00:23:50 +0100

    [Day1][Part2] `quot` peut naturellement donner des valeurs négatives...
    - J'ai bien nettoyé le code de la [Part1] en résuisant les tuples inutiles
    - Je veux apprendre à lancer des exceptions pour les cas `otherwise`
      comme dans rotate qui ne sont pas gérés convenablement (on ignore la
      mauvaise instruction)
    - Les traces pour le debug c'est génial, one remerciera StackOverflow
      pour m'avoir montré comment faire

--- day1/part2/app/Main.hs
+++ day1/part2/app/Main.hs
@@ -1,29 +1,35 @@
 module Main where
 import System.IO
 import Control.Monad()
+import Debug.Trace
 
 main :: IO ()
 main = do
   handle <- openFile "input.txt" ReadMode
   contents <- hGetContents handle
   let singlewords = words contents
-  let (finalPos, zeros) = zeroCount singlewords (50, 0)
+  let (finalPos, zeros) = zeroCount singlewords 50 0
   putStrLn $ "Final position : " ++ show finalPos
   putStrLn $ "Zeros : " ++ show zeros
   hClose handle
 
 
-adjust :: Int -> Int
-adjust n = mod n 100
+adjustAndCount :: Int -> Int -> (Int, Int)
+adjustAndCount o n
+  | n == 0 = (n, 1)
+  | n < 0 && o /= 0 = (mod n 100, 1 + abs (quot n 100))
+  | n > 0 && o < 0 = (mod n 100, 1 + quot n 100)
+  | otherwise = (mod n 100, abs (quot n 100))
 
-rotate :: [Char] -> Int -> Int
-rotate inst pos
-  | head inst == 'L' = adjust (pos - read (tail inst))
-  | head inst == 'R' = adjust (pos + read (tail inst))
-  | otherwise = pos
+rotate :: [Char] -> Int -> (Int, Int)
+rotate instruction pos
+  | head instruction == 'L' = adjustAndCount pos (pos - read (tail instruction))
+  | head instruction == 'R' = adjustAndCount pos (pos + read (tail instruction))
+  | otherwise = (pos, 0)
 
-zeroCount :: [[Char]] -> (Int,  Int) -> (Int, Int)
-zeroCount inst (pos, zeros)
+zeroCount :: [[Char]] -> Int -> Int -> (Int, Int)
+zeroCount inst pos zeros
   | null inst = (pos, zeros)
-  | rotate (head inst) pos == 0 = zeroCount (tail inst) (0, zeros + 1)
-  | otherwise = zeroCount ( tail inst  ) (rotate (head inst) pos, zeros)
+  | otherwise = trace (show pos ++ "/" ++ show zeros) (zeroCount (tail inst) newPos (zeros + addedZeros))
+  where
+    (newPos, addedZeros) = rotate (head inst) pos
