db0a5657349518197f07e2a71dc358bb6c802b66
Valentin Pearce <valentin.pearce@forgetfulmuffin.fr>
2025-12-03 14:44:23 +0100

    [Day3][Part2] Toujours logger, toujours vérifier ses conditions de sortie
    Ah et en fait on peut faire du multiligne après une guard 🤡

--- day3/part2/app/Main.hs
+++ day3/part2/app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 import System.IO
 import Control.Monad()
 import Data.Char ( digitToInt )
+import Debug.Trace
 
 main :: IO ()
 main = do
@@ -15,18 +16,24 @@ main = do
 findTotalJoltage :: [[Char]] -> Int
 findTotalJoltage batteries
   | null batteries = 0
-  | otherwise = read (findBatteryJoltage 12 (head batteries) ++ show (findTotalJoltage (tail batteries)))
+  | otherwise = read (findBatteryJoltage 12 (head batteries)) + findTotalJoltage (tail batteries)
 
 findBatteryJoltage :: Int -> [Char] -> [Char]
-findBatteryJoltage size battery = do
-  let (batteryHead, _) = splitAt (length battery - size) battery
-  let (tensIndex, tens) = findHighest batteryHead
-  let (_, batteryTails) = splitAt (tensIndex +1) battery
-  let units = findBatteryJoltage (size - 1) batteryTails
-  read (tens ++ units)
+findBatteryJoltage size battery
+  | size <=0 = ""
+  | null battery = ""
+  | size == 1 = highest
+  | otherwise = trace ("findBatteryJoltage : " ++ battery) (
+  let (batteryHead, _) = splitAt (length battery - size + 1) battery
+      (tensIndex, tens) = findHighest batteryHead
+      (_, batteryTails) = splitAt (tensIndex +1) battery
+      units = findBatteryJoltage (size - 1) batteryTails
+  in (tens ++ units))
+  where
+    (_, highest) = findHighest battery
 
 findHighest :: [Char] -> (Int, [Char])
-findHighest subBattery = findHighestInner subBattery 0 0 0
+findHighest subBattery = trace ("findHighest : " ++ subBattery) (findHighestInner subBattery 0 0 0)
 
 findHighestInner :: [Char] -> Int -> Int -> Int -> (Int, [Char])
 findHighestInner battery currentIndex currentMaxIndex currentMax
